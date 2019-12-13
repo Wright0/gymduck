43bde9c9ea0d1c043b5a230faeb408eb3685753b
Olivia <olivia.wright101@gmail.com>
2019-12-13 14:30:47 +0000

    initial file setup

---
+++ db/gymduck.sql
@@ -0,0 +1,18 @@
+DROP TABLE members_gym_classes;
+DROP TABLE gym_classes;
+DROP TABLE members;
+
+CREATE TABLE members (
+  id SERIAL primary key,
+  name: VARCHAR(225),
+  age: int,
+
+)
+
+CREATE TABLE gym_classes(
+
+)
+
+CREATE TABLE members_gym_classes (
+
+)
