d757dea212b69c85d4ab2b5a9bdce140dcd0eddb
Olivia <olivia.wright101@gmail.com>
2019-12-13 14:36:54 +0000

    created tables

--- db/gymduck.sql
+++ db/gymduck.sql
@@ -4,15 +4,22 @@ DROP TABLE members;
 
 CREATE TABLE members (
   id SERIAL primary key,
-  name: VARCHAR(225),
-  age: int,
+  name VARCHAR(225),
+  age int,
+  membership_type VARCHAR(255),
+  status VARCHAR(255)
+);
 
-)
-
-CREATE TABLE gym_classes(
-
-)
+CREATE TABLE gym_classes (
+  id SERIAL primary key,
+  name VARCHAR(255),
+  class_type VARCHAR(255),
+  date DATE,
+  time TIME
+);
 
 CREATE TABLE members_gym_classes (
-
-)
+  id SERIAL primary key,
+  member_id INT,
+  gym_class_id INT
+);
