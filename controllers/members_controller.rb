a25a689a7e4fe2ac30e3e8469d0d0a6ae4e877b4
Olivia <olivia.wright101@gmail.com>
2019-12-15 17:10:24 +0000

    Started work on member edit page

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -8,13 +8,23 @@ get '/members' do # show all
   erb(:"members/index")
 end
 
+get '/members/:id/edit' do #Edit existing member
+  @member = Member.find_by_id(params[:id])
+  erb(:"members/edit")
+end
+
 get '/members/:id' do # show member profile
   @member = Member.find_by_id(params[:id])
   erb(:"members/show")
 end
 
-post '/members/:id/deactivate' do
+post '/members/:id/change-status' do #Toggle between active and inactive
   member = Member.find_by_id(params[:id])
   member.set_membership_status
+  redirect to '/members'
+end
+
+post '/members/:id' do #update member information
+  Member.new( params ).update
   redirect to '/members'
 end
